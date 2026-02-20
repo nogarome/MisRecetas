11914b7aabf83244242be161b07092614eece769
Your NameEdu <e.nogales59@yahoo.es>
2026-02-20 23:12:07 +0100

    2º commit

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = 'mis-recetas-v1';
+const CACHE_NAME = 'mis-recetas-v2';
 const ASSETS = [
-  'index.html',
-  'recetas.html',
-  'style.css',
-  'main.css',
-  'pageflip.js',
-  'img/bookgris.png',
-  'img/papel.png',
-  'img/portada.png'
+  './',
+  './index.html',
+  './recetas.html',
+  './style.css',
+  './main.css',
+  './pageflip.js',
+  './manifest.json',
+  './img/bookgris.png',
+  './img/papel.png',
+  './img/portada.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -16,12 +18,39 @@ self.addEventListener('install', (event) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          if (event.request.method === 'GET' && fetchResponse.status === 200) {
+            cache.put(event.request, fetchResponse.clone());
+          }
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
     })
   );
 });
