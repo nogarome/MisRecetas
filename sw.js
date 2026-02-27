e61823bd025fe8bbe7d7190bf1754ace3539abb0
Your NameEdu <e.nogales59@yahoo.es>
2026-02-27 10:04:35 +0100

    mis-recetas-v11

--- sw.js
+++ sw.js
@@ -1,31 +1,31 @@
-const CACHE_NAME = 'mis-recetas-v10';
+const CACHE_NAME = "mis-recetas-v11";
 const ASSETS = [
-  './',
-  './index.html',
-  './recetas.html',
-  './style.css',
-  './main.css',
-  './pageflip.js',
-  './recetario.js',
-  './recetas.json',
-  './manifest.json',
-  './img/bookgris.png',
-  './img/papel.png',
-  './img/portada.png',
-  './img/icon-192.png',
-  './img/icon-512.png'
+  "./",
+  "./index.html",
+  "./recetas.html",
+  "./style.css",
+  "./main.css",
+  "./pageflip.js",
+  "./recetario.js",
+  "./recetas.json",
+  "./manifest.json",
+  "./img/bookgris.png",
+  "./img/papel.png",
+  "./img/portada.png",
+  "./img/icon-192.png",
+  "./img/icon-512.png",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }),
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -33,28 +33,37 @@ self.addEventListener('activate', (event) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((fetchResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          if (event.request.method === 'GET' && fetchResponse.status === 200) {
-            cache.put(event.request, fetchResponse.clone());
-          }
-          return fetchResponse;
-        });
-      });
-    }).catch(() => {
-      if (event.request.mode === 'navigate') {
-        return caches.match('./index.html');
-      }
-    })
+    caches
+      .match(event.request)
+      .then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((fetchResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              if (
+                event.request.method === "GET" &&
+                fetchResponse.status === 200
+              ) {
+                cache.put(event.request, fetchResponse.clone());
+              }
+              return fetchResponse;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      }),
   );
 });
